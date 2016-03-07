afa1d5182dfa2519091418b1a7b818cc5eec5f7d
exenza <massimo.berta@gmail.com>
2016-03-07 21:24:28 +0000

    Finalised timestamp.js

---
+++ timestamp.js
@@ -0,0 +1,34 @@
+var express = require("express")
+var app = express()
+var path = require('path')
+
+function getMonth(m){
+   var mA=["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+   return mA[m]
+}
+
+function evalString(s){
+    s=Date.parse(s)
+    if (!s){
+        s={"unix": null, "natural": null}
+    } else if(!isNaN(s)) {
+        var nDate=new Date(s)
+        var natural=getMonth(nDate.getMonth())+' '+nDate.getDate()+', '+nDate.getFullYear()
+        s={"unix": s, "natural": natural}
+    }
+    return s
+}
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname + '/public/info.html'));
+});
+
+app.get('/*', function (req, res) {
+  var urlString=evalString(req.url)
+  res.type('application/json'); 
+  res.json(urlString)
+});
+
+app.listen(8080, function () {
+  console.log('Timestamp app listening on port 8080!');
+});
